ac18965342200e3bd70c19e1201b1e732c3942e0
Linda <lexicon736@gmail.com>
2022-10-11 14:04:07 -0400

    Finally Complete

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,5 @@
-// import Main from "./pages/Main";
-// import Footer from "./pages/Footer";
-// import Header from "./pages/Header";
-// import Nav from "./pages/Nav";
-
-// export  default function Blog (props) {
-//   return (
-//     <>
-//     <Header />
-//     <Nav />
-//     <Main />
-//     <Footer />
-//     </>
-//   )
-// }
+export  default function Blog (props) {
+  return (
+    <h1>Blog Page!</h1>
+  )
+}
